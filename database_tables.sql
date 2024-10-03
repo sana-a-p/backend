c97a71763fb318478f55530eeb0a51daf914e56b
sana-a-p <136625349+sana-a-p@users.noreply.github.com>
2024-10-03 06:35:40 +0530

    first commit

---
+++ database_tables.sql
@@ -0,0 +1,45 @@
+use fooddelivery;
+CREATE TABLE user (
+    user_id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    phone_no VARCHAR(15),
+    address TEXT
+)AUTO_INCREMENT=100;
+
+CREATE TABLE admin (
+    admin_id INT AUTO_INCREMENT PRIMARY KEY,
+    password VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE menu (
+    menu_id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    price DECIMAL(10, 2) NOT NULL,
+    count INT NOT NULL,
+    admin_id INT,
+    FOREIGN KEY (admin_id) REFERENCES admin(admin_id)
+);
+
+CREATE TABLE orders (
+    order_id INT AUTO_INCREMENT PRIMARY KEY,
+    food_name VARCHAR(100) NOT NULL,
+    address VARCHAR(250) NOT NULL,
+    user_id INT,
+    FOREIGN KEY (user_id) REFERENCES user(user_id)
+) AUTO_INCREMENT = 100;
+
+CREATE TABLE order_details (
+    order_detail_id INT AUTO_INCREMENT PRIMARY KEY,
+    order_id INT,
+    name VARCHAR(100) NOT NULL,
+    price DECIMAL(10, 2) NOT NULL,
+    menu_id INT,
+    count INT NOT NULL,
+    FOREIGN KEY (order_id) REFERENCES orders (order_id),
+    FOREIGN KEY (menu_id) REFERENCES menu(menu_id)
+);
+
+
+show tables;
