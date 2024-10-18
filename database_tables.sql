561345a0c30ccb388de8d5af3d35ece5242d1639
code-lover636 <aravindashokan636@gmail.com>
2024-10-18 23:32:10 +0530

    setup

--- database_tables.sql
+++ database_tables.sql
@@ -19,8 +19,9 @@ CREATE TABLE menu (
     menu_id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
     price DECIMAL(10, 2) NOT NULL,
-    quantity INT NOT NULL,
+    quantity INT NOT NULL default 1,
     admin_id INT,
+    count integer,
     FOREIGN KEY (admin_id) REFERENCES admin(admin_id)
 );
 
